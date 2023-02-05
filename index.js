f640a8a8d2e01faf8704cf7c2cce7c25b1ba98d3
SQrL143 <SQrL143@users.noreply.github.com>
2023-02-04 22:09:01 -0600

    haiku references updated
    Haiku became story
    Haikus be ame stories
    Mina became Clarisaa
    June is untouched

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-const haikus = require('./haikus.json');
+const stories = require('./story.json');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', {stories: stories});
 });
 
 app.listen(port);
