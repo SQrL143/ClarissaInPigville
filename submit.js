153aa82759c98971df56e01d13df7c205149bf3d
SQrL143 <26024995+SQrL143@users.noreply.github.com>
2023-02-09 05:36:36 -0600

    Adding Admin page to manage images and stories

---
+++ submit.js
@@ -0,0 +1,25 @@
+//create a route to handle the form submission
+
+// Path: submit.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+    let storyObject = {
+        title: req.body.title,
+        story: req.body.story,
+        image: req.body.image
+    }
+    let storyJSON = JSON.stringify(storyObject);
+    fs.writeFile('story.json', storyJSON, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        res.send('Thank you for submitting your story!');
+    });
+});
+
+app.listen(3000);
